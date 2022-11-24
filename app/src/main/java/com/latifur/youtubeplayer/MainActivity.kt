50b5ce013f575a329e31b17541b8afbfa5a3cf69
Larry <larry@devstation39.com>
2022-11-24 12:02:20 +0600

    add youtube video player

--- app/src/main/java/com/latifur/youtubeplayer/MainActivity.kt
+++ app/src/main/java/com/latifur/youtubeplayer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.latifur.youtubeplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.latifur.youtubeplayer.databinding.ActivityMainBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var video_id = "Ps4aVpIESkc"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycle.addObserver(binding.videoPlayer)
+
+        binding.apply {
+            videoPlayer.enterFullScreen()
+            videoPlayer.toggleFullScreen()
+
+            videoPlayer.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    youTubePlayer.loadVideo(video_id, 0f)
+                }
+            })
+        }
     }
 }
